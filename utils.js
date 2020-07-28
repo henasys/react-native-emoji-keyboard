a98620e70fb9a2db6d041449f4c394793678a7a1
Alex Hojong Lee <alexhojonglee@gmail.com>
2020-07-28 14:36:58 +0900

    add getUnicodeVersion

--- utils.js
+++ utils.js
@@ -22,8 +22,30 @@ export const isAndroid = () => {
     return Platform.OS === 'android';
 };
 
+const getUnicodeVersion = () => {
+    const version = parseFloat(Platform.Version);
+    if (Platform.OS === 'ios') {
+        return version < 13.2 ? 11 : 12.1;
+    } else {
+        if (version < 24) {
+            return 7.0;
+        } else if (version < 26) {
+            return 8.0;
+        } else if (version < 28) {
+            return 9.0;
+        } else if (version < 29) {
+            return 10.0;
+        } else {
+            return 12.1;
+        }
+    }
+};
+
 export const handleDefaultEmoji = (data, blackList) => {
-    const filteredData = data.filter(e => !_.includes(blackList, e.short_name));
+    const unicodeVersion = getUnicodeVersion();
+    console.log('unicodeVersion', unicodeVersion);
+    const checkedData = data.filter(e => parseFloat(e.added_in) <= unicodeVersion && e[hasImageOS] === true);
+    const filteredData = checkedData.filter(e => !_.includes(blackList, e.short_name));
     const sortedData = _.orderBy(filteredData, 'sort_order');
     const groupedData = _.groupBy(sortedData, 'category');
 
@@ -41,6 +63,7 @@ export const handleDefaultEmoji = (data, blackList) => {
             return {
                 code: toEmoji(value.unified),
                 name: value.short_name,
+                added_in: value.added_in,
                 skins
             };
         })
